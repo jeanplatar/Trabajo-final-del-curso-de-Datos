7f3588d3d3dc4b9a3c7481e224ad7ec334312b07
jeanplatar <jeanplata00@yahoo.com>
2026-07-26 16:12:12 -0300

    actualizacion

--- Mercadolaborar.py/Limpieza.py
+++ Mercadolaborar.py/Limpieza.py
@@ -16,6 +16,7 @@ remote_ratio          0
 company_location      0
 company_size          0"""
 
+#no se encontraron dotos nulos o faltantes
 
 
 print(dataframe.duplicated().sum())
@@ -35,6 +36,7 @@ company_size          0
 dtype: int64
 0
 Duplicados"""
+# NO se encontraron registros Duplicados 
 
 dataframe.rename(columns={
     "wor_year":"Año_de_trabajo",
@@ -73,9 +75,81 @@ print(dataframe.tail())
 print("-----------------------------------------------------")
 
 print(dataframe.info())
-
-
+"""RangeIndex: 607 entries, 0 to 606
+Data columns (total 12 columns):
+ #   Column              Non-Null Count  Dtype
+---  ------              --------------  -----
+ 0   Unnamed: 0          607 non-null    int64
+ 1   work_year           607 non-null    int64
+ 2   experience_level    607 non-null    str  
+ 3   tipo_de_empleo      607 non-null    str  
+ 4   job_title           607 non-null    str  
+ 5   salary              607 non-null    int64
+ 6   salary_currency     607 non-null    str  
+ 7   salary_in_usd       607 non-null    int64
+ 8   employee_residence  607 non-null    str  
+ 9   remote_ratio        607 non-null    int64
+ 10  company_location    607 non-null    str  
+ 11  company_size        607 non-null    str  
+dtypes: int64(5), str(7)
+memory usage: 57.0 KB
+None"""
+# los datos eran correctos y no fue necesario modificarlos
+# ademas  la colimna " Unnamed 0", se elimino por que fue un indice generado al exportar el archivo
 print("---------------------------------------------------------" )
 
-
 print(dataframe.describe())
+
+"""       Unnamed: 0    work_year        salary  salary_in_usd  remote_ratio
+count  607.000000   607.000000  6.070000e+02     607.000000     607.00000
+mean   303.000000  2021.405272  3.240001e+05  112297.869852      70.92257
+std    175.370085     0.692133  1.544357e+06   70957.259411      40.70913
+min      0.000000  2020.000000  4.000000e+03    2859.000000       0.00000
+25%    151.500000  2021.000000  7.000000e+04   62726.000000      50.00000
+50%    303.000000  2022.000000  1.150000e+05  101570.000000     100.00000
+75%    454.500000  2022.000000  1.650000e+05  150000.000000     100.00000
+max    606.000000  2022.000000  3.040000e+07  600000.000000     100.00000
+
+"""
+print("---------------------------------------------------------------------")
+
+print(dataframe.dtypes)
+
+"""Unnamed: 0            int64
+work_year             int64
+experience_level        str
+tipo_de_empleo          str
+job_title               str
+salary                int64
+salary_currency         str
+salary_in_usd         int64
+employee_residence      str
+remote_ratio          int64
+company_location        str
+company_size            str
+dtype: object"""
+print("------------------------------------------------------------------")
+
+dataframe.drop("Unnamed: 0", axis=1,inplace=True)
+"""<class 'pandas.DataFrame'>
+RangeIndex: 607 entries, 0 to 606
+Data columns (total 11 columns):
+ #   Column              Non-Null Count  Dtype
+---  ------              --------------  -----
+ 0   work_year           607 non-null    int64
+ 1   experience_level    607 non-null    str  
+ 2   tipo_de_empleo      607 non-null    str  
+ 3   job_title           607 non-null    str  
+ 4   salary              607 non-null    int64
+ 5   salary_currency     607 non-null    str  
+ 6   salary_in_usd       607 non-null    int64
+ 7   employee_residence  607 non-null    str  
+ 8   remote_ratio        607 non-null    int64
+ 9   company_location    607 non-null    str  
+ 10  company_size        607 non-null    str  
+dtypes: int64(4), str(7)
+memory usage: 52.3 KB
+None"""
+
+
+print(dataframe.info())
